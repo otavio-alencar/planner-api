d5eb63fbe7082bb75d2cc590a4b27588fc846203
Luana Siqueira de Sousa <luana.siqueira@ufape.edu.br>
2026-07-16 17:44:38 -0300

    refactor(categoria): move controller para namespace Api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\Api\CategoriaController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/registrar', [AuthController::class, 'registrar']);
